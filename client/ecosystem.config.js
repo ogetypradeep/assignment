7d932102e9dcd0e02711d6e27bcd92cd531c28dc
ogetypradeep <rangapradeep.ogety@gmail.com>
2023-04-10 07:22:53 +0530

    commiting changes

--- client/ecosystem.config.js
+++ client/ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps : [{
     name   : "todo-list-client",
     script : "./node_modules/react-scripts/bin/react-scripts.js",
-    args: "start",
+    args: "--openssl-legacy-provider start",
     env: {
       "NODE_ENV": "development",
     }
